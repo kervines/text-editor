91d1b8576ccbc410056911b93e13be10d8977768
kervines <kervinzero@gmail.com>
2024-05-01 11:36:33 -0300

    refatoring code - retirando propriedade sem utilidade

--- js/script.js
+++ js/script.js
@@ -7,25 +7,20 @@ const btnUnderline = document.querySelector('.underline');
 const buttons = document.querySelectorAll('.edit-container button');
 
 const textConfigUpdate = JSON.parse(localStorage.getItem('text')) ?? '';
-textContent.innerHTML =
-  `<span class='${textConfigUpdate.fontWeight} ${textConfigUpdate.fontStyle} ${textConfigUpdate.fontSize}'> ${textConfigUpdate.content} </span>` ??
-  '';
+textContent.innerHTML = `<span class='${textConfigUpdate.fontSize}'> ${
+  textConfigUpdate.content ?? ''
+} </span>`;
 
 const textConfig = {
   content: textConfigUpdate.content ?? '',
-  fontWeight: textConfigUpdate.fontWeight ?? '',
-  fontStyle: textConfigUpdate.fontStyle ?? '',
   fontSize: textConfigUpdate.fontSize ?? '',
-  textDecoration: textConfigUpdate.textDecoration ?? '',
   fontColor: textConfigUpdate.fontColor ?? '',
 };
 
 const handleKeyup = (e) => {
-  // textConfig.content = `<span class='${textConfig.fontWeight} ${textConfig.fontStyle} ${textConfig.fontSize}'>${textContent.innerText} </span>`;
   textConfig.content = textContent.innerText;
   localStorage.setItem('text', JSON.stringify(textConfig));
 };
-
 textContent.addEventListener('keyup', handleKeyup);
 
 buttons.forEach((button) => {
